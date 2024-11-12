b0f56e8f46970181b068a4840e3fbcc7fc827570
pabloRedolar <predolars@iesch.org>
2024-11-12 18:50:15 +0100

    a

--- practica5-UD1-TiendaOnline/src/Ejer3sumaRecursiva.java
+++ practica5-UD1-TiendaOnline/src/Ejer3sumaRecursiva.java
@@ -1,41 +1,65 @@
+import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
-import java.util.Arrays;
 
-public class Ejer3sumaRecursiva extends RecursiveTask<Integer> {
-    final static int TOTAL = 20;
-    final static int THRESHOLD = 5;
+public class Ejer3sumaRecursiva extends RecursiveTask<Long> {
+    final static long TOTAL = 200000000;
+    final static long THRESHOLD = 5000000;
 
-    int[] numbers;
-    int start;
-    int length;
+    long[] numbers;
+    long start;
+    long length;
 
-    public Ejer3sumaRecursiva(int[] numbers, int start, int length) {
+    public Ejer3sumaRecursiva(long[] numbers, long start, long length) {
         this.numbers = numbers;
         this.start = start;
         this.length = length;
     }
 
-    private int sumIterative() {
-        int sum = 0;
-        int end = start + length;
-        for (int i = start; i < end; i++) {
-            sum += numbers[i];
+    static long[] generateArray() {
+        long[] numbers = new long[(int) TOTAL];
+        for (long i = 0; i < TOTAL; i++) {
+            numbers[(int) i] = i + 1;
         }
-        System.out.println("Suma iterativa de " + Arrays.toString(Arrays.copyOfRange(numbers, start, end)) + " = " + sum);
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        long[] numbers = generateArray();
+
+        Ejer3sumaRecursiva task = new Ejer3sumaRecursiva(numbers, 0, TOTAL);
+        long start = System.currentTimeMillis();
+        long sumIterative = task.sumIterative();
+        long end = System.currentTimeMillis();
+        System.out.println("Sum (" + sumIterative + ") iterative found in " + (end - start) + " ms");
+
+        ForkJoinPool pool = new ForkJoinPool();
+        start = System.currentTimeMillis();
+        long sumParallel = Math.toIntExact(pool.invoke(new Ejer3sumaRecursiva(numbers, 0, TOTAL)));
+        end = System.currentTimeMillis();
+        System.out.println("Sum (" + sumParallel + ") parallel found in " + (end - start) + " ms");
+    }
+
+    private long sumIterative() {
+        long sum = 0;
+        long end = start + length;
+        for (long i = start; i < end; i++) {
+            sum += numbers[(int) i];
+        }
+        System.out.println("Suma iterativa de " + Arrays.toString(Arrays.copyOfRange(numbers, (int) start, (int) end)) + " = " + sum);
         return sum;
     }
 
-    private int sumRecursive() {
-        int mid = length / 2;
+    private long sumRecursive() {
+        long mid = length / 2;
         Ejer3sumaRecursiva task1 = new Ejer3sumaRecursiva(numbers, start, mid);
         Ejer3sumaRecursiva task2 = new Ejer3sumaRecursiva(numbers, start + mid, length - mid);
 
-        System.out.println("Dividiendo: " + Arrays.toString(Arrays.copyOfRange(numbers, start, start + length)));
+        System.out.println("Dividiendo: " + Arrays.toString(Arrays.copyOfRange(numbers, (int) start, (int) (start + length))));
 
         task1.fork();
-        int resultRight = task2.compute();
-        int resultLeft = task1.join();
+        Long resultRight = task2.compute();
+        Long resultLeft = task1.join();
 
         System.out.println(resultLeft);
         System.out.println(resultRight);
@@ -44,35 +68,11 @@ public class Ejer3sumaRecursiva extends RecursiveTask<Integer> {
     }
 
     @Override
-    protected Integer compute() {
+    protected Long compute() {
         if (length > THRESHOLD) {
             return sumRecursive();
         } else {
             return sumIterative();
         }
-    }
-
-    static int[] generateArray() {
-        int[] numbers = new int[TOTAL];
-        for (int i = 0; i < TOTAL; i++) {
-            numbers[i] = i + 1;
-        }
-        return numbers;
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = generateArray();
-
-        Ejer3sumaRecursiva task = new Ejer3sumaRecursiva(numbers, 0, TOTAL);
-        long start = System.currentTimeMillis();
-        int sumIterative = task.sumIterative();
-        long end = System.currentTimeMillis();
-        System.out.println("Sum (" + sumIterative + ") iterative found in " + (end - start) + " ms");
-
-        ForkJoinPool pool = new ForkJoinPool();
-        start = System.currentTimeMillis();
-        int sumParallel = pool.invoke(new Ejer3sumaRecursiva(numbers, 0, TOTAL));
-        end = System.currentTimeMillis();
-        System.out.println("Sum (" + sumParallel + ") parallel found in " + (end - start) + " ms");
     }
 }
